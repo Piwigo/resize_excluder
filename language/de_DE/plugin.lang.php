f377adb15da5e5fad4a82964690fcca3838e75a7
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-05-15 13:45:35 +0200

    Update de_DE, thanks to slim

---
+++ language/de_DE/plugin.lang.php
@@ -0,0 +1,9 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Which group should be excluded from the resize on upload setting ?'] = 'Welche Gruppe sollte von der Einstellung „Größe beim Hochladen ändern“ ausgeschlossen werden?';
+$lang['You need to have at least one groupe to use this setting.'] = 'Sie müssen mindestens eine Gruppe haben, um diese Einstellung zu verwenden';
