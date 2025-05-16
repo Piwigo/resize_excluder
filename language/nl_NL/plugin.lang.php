d4651f642117db8c32cd04eff6378326f2c593d0
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-05-16 20:55:30 +0200

    Update nl_NL, thanks to watertoren5044

---
+++ language/nl_NL/plugin.lang.php
@@ -0,0 +1,9 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Which group should be excluded from the resize on upload setting ?'] = 'Welke groep moet worden uitgesloten van de instelling \'formaat wijzigen bij uploaden\'?';
+$lang['You need to have at least one groupe to use this setting.'] = 'U moet minimaal één groep hebben om deze instelling te kunnen gebruiken';
