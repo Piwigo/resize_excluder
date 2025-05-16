e7a5e6af1eb316ada5749dee4a89f8ce521d3ba1
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-05-16 08:44:08 +0200

    Update ca_ES, thanks to Tirs

---
+++ language/ca_ES/plugin.lang.php
@@ -0,0 +1,9 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Which group should be excluded from the resize on upload setting ?'] = 'Quin grup s\'ha d\'excloure del paràmetre de canvi de mida al carregar imatges?';
+$lang['You need to have at least one groupe to use this setting.'] = 'Necessiteu almenys un grup per utilitzar aquest paràmetre';
