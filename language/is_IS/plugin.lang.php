dc453aad0950337d5b2a127f04e2ff361c1d7a15
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-05-16 08:55:53 +0200

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/plugin.lang.php
@@ -0,0 +1,9 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Which group should be excluded from the resize on upload setting ?'] = 'Hvaða hópur ætti að vera undanskilinn frá stillingunni fyrir stærðarbreytingu við upphleðslu?';
+$lang['You need to have at least one groupe to use this setting.'] = 'Þú þarft að hafa að minnsta kosti einn hóp til að nota þessa stillingu.';
