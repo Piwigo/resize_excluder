1e6fe9c9d432362d59c9dbe66460406ca46b39b3
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-05-16 08:44:14 +0200

    Update pt_BR, thanks to flaviove

---
+++ language/pt_BR/plugin.lang.php
@@ -0,0 +1,9 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Which group should be excluded from the resize on upload setting ?'] = 'Qual grupo deve ser excluído da configuração de redimensionamento no upload?';
+$lang['You need to have at least one groupe to use this setting.'] = 'Você precisa ter pelo menos um grupo para usar esta configuração';
