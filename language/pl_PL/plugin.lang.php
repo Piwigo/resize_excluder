67490dcda568e169dca8aa6b5ec22a20af70c594
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-05-16 08:44:17 +0200

    Update pl_PL, thanks to yareq

---
+++ language/pl_PL/plugin.lang.php
@@ -0,0 +1,9 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Which group should be excluded from the resize on upload setting ?'] = 'Która grupa powinna zostać wykluczona z ustawień zmiany rozmiaru po przesłaniu?';
+$lang['You need to have at least one groupe to use this setting.'] = 'Musisz mieć co najmniej jedną grupę, aby skorzystać z tego ustawienia';
