735eef19665a60f6d190498460b3bf460ab1f798
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-05-16 08:44:20 +0200

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/plugin.lang.php
@@ -0,0 +1,9 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Which group should be excluded from the resize on upload setting ?'] = 'Hvilken gruppe skal ekskluderes fra størrelsesændring ved upload-indstillingen?';
+$lang['You need to have at least one groupe to use this setting.'] = 'Du skal have mindst én gruppe, for at benytte denne indstilling';
