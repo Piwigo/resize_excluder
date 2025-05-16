0060bf6d8083a747f33bc0a8ab8953214ff05d09
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-05-16 08:44:06 +0200

    Update sk_SK, thanks to dodo

---
+++ language/sk_SK/plugin.lang.php
@@ -0,0 +1,9 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Which group should be excluded from the resize on upload setting ?'] = 'Ktorá skupina by mala byť vylúčená z nastavenia zmeny veľkosti pri nahrávaní?';
+$lang['You need to have at least one groupe to use this setting.'] = 'Ak chcete použiť toto nastavenie, musíte mať aspoň jednu skupinu.';
