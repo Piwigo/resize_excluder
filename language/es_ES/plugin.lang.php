77c754313b4122dc3f63a634acd5bf99355fc54e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-05-16 08:44:11 +0200

    Update es_ES, thanks to Tirs

---
+++ language/es_ES/plugin.lang.php
@@ -0,0 +1,9 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Which group should be excluded from the resize on upload setting ?'] = '¿Qué grupo debería excluirse del parametro de cambio de tamaño al subir imágenes?';
+$lang['You need to have at least one groupe to use this setting.'] = 'Necesita tener por lo menos un grupo para usar este parámetro';
