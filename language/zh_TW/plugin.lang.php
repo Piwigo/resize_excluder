2de29f46fc7f095cc37a5075e72f575e5e3369e9
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-05-20 10:50:09 +0200

    Update zh_TW, thanks to closer

---
+++ language/zh_TW/plugin.lang.php
@@ -0,0 +1,9 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Which group should be excluded from the resize on upload setting ?'] = '上載時調整大小設定應排除哪個群組？';
+$lang['You need to have at least one groupe to use this setting.'] = '欲使用此設定，您至少要選擇一個群組';
