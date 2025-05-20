c7a603f3eb7ce4e575e903a54cc68f66dd7a829e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-05-20 05:59:47 +0200

    Update he_IL, thanks to epifeny

---
+++ language/he_IL/plugin.lang.php
@@ -0,0 +1,9 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Which group should be excluded from the resize on upload setting ?'] = 'איזו קבוצה צריכה להיות מוחרגת מהגדרת שינוי הגודל בעת ההעלאה?';
+$lang['You need to have at least one groupe to use this setting.'] = 'עליך להגדיר לפחות קבוצה אחת כדי להשתמש בהגדרה הזו';
