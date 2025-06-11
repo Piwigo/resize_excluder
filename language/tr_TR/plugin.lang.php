eca34f7e503f6fa31ba1da0b393a0450e7e432db
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-06-11 06:33:15 +0200

    Update tr_TR, thanks to LazBoy

---
+++ language/tr_TR/plugin.lang.php
@@ -0,0 +1,9 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Which group should be excluded from the resize on upload setting ?'] = 'Yüklemelerde yeniden boyutlandırma ayarından hangi grup hariç tutulmalıdır?';
+$lang['You need to have at least one groupe to use this setting.'] = 'Bu ayarı kullanmak için en az bir gruba sahip olmanız gerekir';
