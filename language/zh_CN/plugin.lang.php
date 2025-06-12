73c13061475e174f3d30b02cb229314a2007ca78
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-06-12 06:44:00 +0200

    Update zh_CN, thanks to walle

---
+++ language/zh_CN/plugin.lang.php
@@ -0,0 +1,9 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Which group should be excluded from the resize on upload setting ?'] = '在设置上传时自动调整图片尺寸的功能时，应该排除哪个用户组？';
+$lang['You need to have at least one groupe to use this setting.'] = '您至少需要选择一个用户组才能使用此设置';
