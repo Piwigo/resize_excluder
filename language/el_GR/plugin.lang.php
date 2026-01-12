1bb85b5d2a9b20d2e8725209425ab50aa0a37e80
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-01-12 06:39:51 +0100

    Update el_GR, thanks to bas_alba

---
+++ language/el_GR/plugin.lang.php
@@ -0,0 +1,9 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Which group should be excluded from the resize on upload setting ?'] = 'Ποια ομάδα πρέπει να εξαιρεθεί από τη ρύθμιση αλλαγής μεγέθους κατά την μεταφόρτωση;';
+$lang['You need to have at least one groupe to use this setting.'] = 'Πρέπει να έχετε τουλάχιστον μία ομάδα για να χρησιμοποιήσετε αυτήν τη ρύθμιση';
